cef8d0aebeac3d81249c9efe4bbf488fa7e616a1
Jason Ronallo <jronallo@gmail.com>
2010-12-17 17:13:01 -0500

    add test for container_types

--- spec/did_spec.rb
+++ spec/did_spec.rb
@@ -7,5 +7,8 @@ describe "Did" do
   it "should be present" do
     @archdesc_did.should be_a_kind_of(Eadsax::Did)
   end
+  it 'should have container types when present' do
+    @ead.archdesc.dsc.c01s.first.c02s.first.did.container_types.should == ['Box', 'Folder']
+  end
 end
 
